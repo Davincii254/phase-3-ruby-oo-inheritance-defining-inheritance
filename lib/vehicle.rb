67f774abf3c074517ec0c6bd39d5a90c24d7ae73
Davincii254 <victorkuria7@gmail.com>
2022-06-11 09:36:26 +0300

    Completed

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,19 @@
 class Vehicle
 
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+  
+    def go
+      "electric cars are silent...!"
+    end
+  
+    def fill_up_tank
+      "YUmmy!"
+    end
+
+
 end
